07aa7460e511c6c6dca48846388a574b2cd1f126
Jelle Klaver <jelle@stekz.com>
2024-10-17 00:06:59 +0200

    feat: introduce a facilitator which manages the focus group in stead of running it sequential

--- src/mystekz_focus_group/crew.py
+++ src/mystekz_focus_group/crew.py
@@ -1,11 +1,10 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+# from .tools.ask_human import AskHuman
 
-# Uncomment the following line to use an example of a custom tool
-# from mystekz_focus_group.tools.custom_tool import MyCustomTool
+def main_point_str(agent: str):
+    return f"{agent}'s main point: [Add the main point of {agent} here]"
 
-# Check our tools documentations for more information on how to use them
-# from crewai_tools import SerperDevTool
 
 @CrewBase
 class MystekzFocusGroupCrew():
@@ -15,30 +14,57 @@ class MystekzFocusGroupCrew():
     def business_consultant(self) -> Agent:
         return Agent(
             config=self.agents_config['business_consultant'],
-            # tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
-            verbose=True
+            verbose=True,
+            # tools=[AskHuman()],
         )
 
     @agent
     def project_manager(self) -> Agent:
         return Agent(
             config=self.agents_config['project_manager'],
-            verbose=True
+            verbose=True,
+            # tools=[AskHuman()],
+        )
+
+    @agent
+    def facilitator(self) -> Agent:
+        return Agent(
+            config=self.agents_config['facilitator'],
+            verbose=True,
         )
 
     @agent
     def summarizer(self) -> Agent:
         return Agent(
             config=self.agents_config['summarizer'],
-            verbose=True
+            verbose=True,
+            allow_delegation=False,
         )
 
-    @agent
-    def discussion_moderator(self) -> Agent:
-        return Agent(
-            config=self.agents_config['discussion_moderator'],
-            verbose=True
+    # @agent
+    # def discussion_moderator(self) -> Agent:
+    #     return Agent(
+    #         config=self.agents_config['discussion_moderator'],
+    #         verbose=True,
+    #     )
+
+    @task
+    def discuss_idea(self) -> Task:
+        agents = [
+            self.business_consultant(),
+            self.project_manager(),
+        ]
+
+        return Task(
+            config=self.tasks_config['discuss_idea'],
+            output_file="output/discussion.md",
+            description=f"""
+                Discuss the following idea regarding our application MyStekz with all participants: {[a.role for a in agents]}. Make sure you convey the whole idea to each participant and that they focus on the idea itself. They should not give feedback on MyStekz overall or how the idea will be implemented.
+                The idea to discuss is: {{idea}}.
+            """,
+            expected_output="""A summary of the discussion.""",
         )
+
 
     @task
     def respond_to_idea_business_consultant(self) -> Task:
@@ -51,30 +77,54 @@ class MystekzFocusGroupCrew():
     def respond_to_idea_project_manager(self) -> Task:
         return Task(
             config=self.tasks_config['respond_to_idea_project_manager'],
-            input_file="output/business_consultant_response.md",
             output_file="output/project_manager_response.md",
         )
 
     @task
     def summarize(self) -> Task:
+        agents = [
+            self.business_consultant(),
+            self.project_manager(),
+        ]
+        agent_strs = [main_point_str(a.role) for a in agents]
+
+        # agent_strs = map(lambda agent: main_point_str(agent.role), [
+        #     self.business_consultant,
+        #     self.project_manager,
+        # ])
         return Task(
             config=self.tasks_config['summarize'],
+            expected_output=f"""
+                List the output for each agent like so:
+                {"\n".join(agent_strs)}
+
+                Then list the key points of agreement.
+                Then list the key points of disagreement.
+            """,
             output_file="output/summary.md",
         )
 
     @crew
     def crew(self) -> Crew:
-        """Creates the MystekzFocusGroup crew."""
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
+            agents=[
+                self.business_consultant(),
+                self.project_manager(),
+                self.summarizer(),
+            ],
+            # tasks=[
+            #     self.respond_to_idea_business_consultant(),
+            #     self.respond_to_idea_project_manager(),
+            # ],
             tasks=[
-                self.respond_to_idea_business_consultant(),
-                self.respond_to_idea_project_manager(),
-                self.discuss_ideas(),
+                self.discuss_idea(),
                 self.summarize(),
             ],
-            process=Process.sequential,
+            # tools=[ask_human],
+            manager_agent=self.facilitator(),
+            process=Process.hierarchical,
             verbose=True,
-            full_output=True,
             share_crew=False,
+            memory=True,
+            planning=True,
         )
